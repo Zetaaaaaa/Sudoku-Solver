bf1b00e02fcf044583d719e9f4259e19ef08dc8f
Zetaaaaaa <95016952+Zetaaaaaa@users.noreply.github.com>
2024-10-20 03:57:15 +0530

    Add files via upload

---
+++ sudoku_solver.py
@@ -0,0 +1,59 @@
+import time
+
+sudoku_board = [
+    [3, 0, 6, 5, 0, 8, 4, 0, 0],
+    [5, 2, 0, 0, 0, 0, 0, 0, 0],
+    [0, 8, 7, 0, 0, 0, 0, 3, 1],
+    [0, 0, 3, 0, 1, 0, 0, 8, 0],
+    [9, 0, 0, 8, 6, 3, 0, 0, 5],
+    [0, 5, 0, 0, 9, 0, 6, 0, 0],
+    [1, 3, 0, 0, 0, 0, 2, 5, 0],
+    [0, 0, 0, 0, 0, 0, 0, 7, 4],
+    [0, 0, 5, 2, 0, 6, 3, 0, 0]
+]
+
+def print_board(board):
+    for row in board:
+        print(" ".join(str(num) if num != 0 else "." for num in row))
+    print()
+
+def is_valid(board, row, col, num):
+    for i in range(9):
+        if board[row][i] == num:
+            return False
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+    start_row = row - row % 3
+    start_col = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if board[i + start_row][j + start_col] == num:
+                return False
+    return True
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        print_board(board)
+                        time.sleep(0.01)
+                        if solve_sudoku(board):
+                            return True
+                        board[row][col] = 0
+                        print_board(board)
+                        time.sleep(0.01)
+                return False
+    return True
+
+print("Original Sudoku:")
+print_board(sudoku_board)
+
+if solve_sudoku(sudoku_board):
+    print("Solved Sudoku:")
+    print_board(sudoku_board)
+else:
+    print("No solution exists")
